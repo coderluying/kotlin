5f6ec75691e84b78b2972b208db240d42c7f12f7
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2017-06-15 19:05:23 +0300

    ~ introduce proto js tests

--- jps-plugin/jps-tests/test/org/jetbrains/kotlin/jps/incremental/AbstractProtoComparisonTest.kt
+++ jps-plugin/jps-tests/test/org/jetbrains/kotlin/jps/incremental/AbstractProtoComparisonTest.kt
@@ -19,19 +19,118 @@ package org.jetbrains.kotlin.jps.incremental
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.testFramework.UsefulTestCase
 import com.intellij.util.SmartList
+import junit.framework.TestCase
+import org.jetbrains.kotlin.cli.common.ExitCode
+import org.jetbrains.kotlin.cli.common.arguments.K2JSCompilerArguments
+import org.jetbrains.kotlin.cli.js.K2JSCompiler
+import org.jetbrains.kotlin.config.Services
 import org.jetbrains.kotlin.incremental.LocalFileKotlinClass
+import org.jetbrains.kotlin.incremental.ProtoCompareGenerated
 import org.jetbrains.kotlin.incremental.difference
 import org.jetbrains.kotlin.incremental.storage.ProtoMapValue
+import org.jetbrains.kotlin.incremental.utils.TestMessageCollector
+import org.jetbrains.kotlin.js.config.JsConfig
+import org.jetbrains.kotlin.js.facade.K2JSTranslator
+import org.jetbrains.kotlin.js.incremental.IncrementalJsService
+import org.jetbrains.kotlin.js.incremental.IncrementalJsServiceImpl
 import org.jetbrains.kotlin.load.kotlin.KotlinJvmBinaryClass
 import org.jetbrains.kotlin.load.kotlin.header.KotlinClassHeader
+import org.jetbrains.kotlin.name.ClassId
+import org.jetbrains.kotlin.serialization.ProtoBuf
+import org.jetbrains.kotlin.serialization.deserialization.NameResolverImpl
 import org.jetbrains.kotlin.serialization.jvm.BitEncoding
 import org.jetbrains.kotlin.test.KotlinTestUtils
 import org.jetbrains.kotlin.test.MockLibraryUtil
 import org.jetbrains.kotlin.utils.Printer
+import org.junit.Assert
 import java.io.File
+import java.util.*
+
+abstract class AbstractJsProtoComparisonTest : AbstractProtoComparisonTest() {
+    override fun doTest(testDataPath: String) {
+        val testDir = KotlinTestUtils.tmpDir("testDirectory")
+        val oldClassFiles = compileFileAndGetClasses(testDataPath, testDir, "old")
+        val newClassFiles = compileFileAndGetClasses(testDataPath, testDir, "new")
+        val oldParts = oldClassFiles.packageParts
+
+        val newParts = newClassFiles.packageParts
+
+        val sb = StringBuilder()
+        val p = Printer(sb)
+
+        comparePackageParts(oldParts.first().proto, newParts.first().proto)
+        val c = 0
+    }
+
+    fun comparePackageParts(old: ProtoBuf.PackageFragment, new: ProtoBuf.PackageFragment) {
+        val oldNameResolver = NameResolverImpl(old.strings, old.qualifiedNames)
+        val newNameResolver = NameResolverImpl(new.strings, new.qualifiedNames)
+        val compare = ProtoCompareGenerated(oldNameResolver, newNameResolver)
+
+        val packageDiff = compare.difference(old.`package`, old.`package`)
+
+        val oldClasses = old.class_List.map { oldNameResolver.getClassId(it.fqName) to it }.toMap()
+        val newClasses = new.class_List.map { newNameResolver.getClassId(it.fqName) to it }.toMap()
+
+        val addedClasses = arrayListOf<ClassId>()
+        val removedClasses = arrayListOf<ClassId>()
+        val modifiedClasses = hashMapOf<ClassId, EnumSet<ProtoCompareGenerated.ProtoBufClassKind>>()
+
+        for (classId in oldClasses.keys + newClasses.keys) {
+            val oldProto = oldClasses[classId]
+            val newProto = newClasses[classId]
+
+            when {
+                oldProto == null -> {
+                    addedClasses.add(classId)
+                }
+                newProto == null -> {
+                    removedClasses.add(classId)
+                }
+                else -> {
+                    modifiedClasses[classId] = compare.difference(oldProto, newProto)
+                }
+            }
+        }
+
+        val c = 0
+    }
+
+    private fun compileFileAndGetClasses(testPath: String, testDir: File, prefix: String): IncrementalJsServiceImpl {
+        val files = File(testPath).listFiles { it -> it.name.startsWith(prefix) }!!
+        val sourcesDirectory = testDir.createSubDirectory("sources")
+        val outputDir = testDir.createSubDirectory("out-$prefix")
+        val sourcesToCompile = arrayListOf<String>()
+
+        files.forEach { file ->
+            val targetFile = File(sourcesDirectory, file.name.replaceFirst(prefix, "main"))
+            FileUtil.copy(file, targetFile)
+            sourcesToCompile.add(targetFile.canonicalPath)
+        }
+
+        val incrementalService = IncrementalJsServiceImpl()
+        // todo: find out if it is safe to call directly
+        val services = Services.Builder().run {
+            register(IncrementalJsService::class.java, incrementalService)
+            build()
+        }
+
+        val messageCollector = TestMessageCollector()
+        val args = K2JSCompilerArguments().apply {
+            outputFile = File(outputDir, "out.js").canonicalPath
+            metaInfo = true
+            freeArgs.addAll(sourcesToCompile)
+        }
+        val exitCode = K2JSCompiler().exec(messageCollector, services, args)
+        val expectedOutput = "OK"
+        val actualOutput = (listOf(exitCode.name) + messageCollector.errors).joinToString("\n")
+        Assert.assertEquals(expectedOutput, actualOutput)
+        return incrementalService
+    }
+}
 
 abstract class AbstractProtoComparisonTest : UsefulTestCase() {
-    fun doTest(testDataPath: String) {
+    open  fun doTest(testDataPath: String) {
         val testDir = KotlinTestUtils.tmpDir("testDirectory")
 
         val oldClassFiles = compileFileAndGetClasses(testDataPath, testDir, "old")
@@ -121,7 +220,7 @@ abstract class AbstractProtoComparisonTest : UsefulTestCase() {
         println("changes in ${oldClass.classId}: ${changes.joinToString()}")
     }
 
-    private fun File.createSubDirectory(relativePath: String): File {
+    protected fun File.createSubDirectory(relativePath: String): File {
         val directory = File(this, relativePath)
         FileUtil.createDirectory(directory)
         return directory
